0b4996d4113eb2374e8f23d5df0c996a0ee1cb39
Ludkao <l.leandro_abreu@hotmail.com>
2021-03-19 00:33:23 -0300

    configuracao inical do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import {Container, Content} from './styles'
 import logo from '../../assets/logo.svg'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logo} alt="logo"/>
-                <button type='button'>Nova transacao</button>
+                <button type='button' onClick={onOpenNewTransactionModal}>Nova transacao</button>
             </Content>
+           
         </Container>
     )
 }
